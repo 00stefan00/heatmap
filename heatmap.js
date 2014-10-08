06519e35a49564108a404dc84074f8152300d884
Stefan <stefan.strijker@outlook.com>
2014-10-08 15:12:49 +0200

    Cleanup and rewrote code to work with GoogleMaps heatlayer instead of Heatmap.js

--- heatmap.js
+++ heatmap.js
@@ -1,102 +1,58 @@
 
-//
-//      Heatmap javascript
-//
-//
-var heatLayerData = [];
-var map;
-var heatLayer;
+
+var map, pointArray, heatmap;
+
+var heatMapData = [];
 
 function initialize() {
-    map = getMap(getMapOptions());
-    google.maps.event.addListener(map, 'idle', onIdle);
-    addHeatLayerData(triggers_hoogeveen);
-    addHeatLayerData(triggers_hoogkerk);
-    //getController();
-}
-    
-
-function onIdle(){
-    getNewHeatLayer(map);
-}
-
-function getMapOptions() {
     var hoogeveen = new google.maps.LatLng(52.73400225036641, 6.47296751087904);
     var hoogkerk = new google.maps.LatLng(53.1982171153152, 6.512751840054989);
     var nijmegen = new google.maps.LatLng(51.82730272563614, 5.824586860835552);
 
     var mapOptions = {
-            center: hoogkerk,
-            zoom: 14,
-            minZoom: 14,
-            maxZoom: 20,
-            mapTypeId: google.maps.MapTypeId.HYBRID
-        };
-    
-    return mapOptions;
-}
-
-function getMap() {
-    return new google.maps.Map(document.getElementById("map-canvas"), getMapOptions());
-}
-
-// function getController(){
-//     var btn = document.createElement("Left_Button");
-//     var t = document.createTextNode("<");
-//     btn.appendChild(t);
-//     document.body.appendChild(btn)
-// }
-
-function getNewHeatLayer(map, data) {
-    var heatLayer = createHeatLayer(map, getData(heatLayerData));
-    heatLayers.push(heatLayer);
-    return heatLayer
-}
-    
-
-function createHeatLayer(map, data){
-    heatLayer = new HeatmapOverlay(map,
-        {
-            "radius": 0.00020,
-            "scaleRadius": true,
-            "maxOpacity": 60,
-            "useLocalExtrema": false,
-            latField: 'lat',
-            lngField: 'lon',
-            valueField: 'triggers'
-        }
-    );
-
-    heatLayer.setData(data);
-}
-
-function addHeatLayerData(data){
-    for (var i = 0; i < data.length; i++) {
-        heatLayerData.push(data[i]);
+        zoom: 10,
+        center: hoogeveen,
+        mapTypeId: google.maps.MapTypeId.SATELLITE
     };
+
+    map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+    console.log("nijmegen");
+    generateHeatMapData(triggers_nijmegen);
+    console.log("hoogeveen");
+    generateHeatMapData(triggers_hoogeveen);
+    console.log("hoogkerk");
+    generateHeatMapData(triggers_hoogkerk);
+
+    pointArray = new google.maps.MVCArray(heatMapData);
+
+    heatmap = new google.maps.visualization.HeatmapLayer({
+        data: pointArray
+    });
+
+    heatmap.setMap(map);
 }
 
-function getData(json) {
-    jsonData = []
-    dataMax = getMedian(json)*6;
+function generateHeatMapData(json) {
+    var median = getMedian(json);
+
     for (i = 0; i < json.length-1; i++){
         
-        longitude = json[i].lon
-        latitude = json[i].lat
+        longitude = json[i].lat
+        latitude = json[i].lon
         triggers_per_hour = json[i].triggers
+        if(triggers_per_hour > median*8) {
+            console.log(triggers_per_hour + " > " + median*8);
+            triggers_per_hour = median*8;
 
-        if(triggers_per_hour > dataMax){
-            triggers_per_hour = dataMax;
         }
 
-        jsonData.push({lat:longitude, lon:latitude, triggers:triggers_per_hour})
+        var dataObject = {
+            location: new google.maps.LatLng(latitude, longitude), 
+            weight: triggers_per_hour
+        }
+        heatMapData.push(dataObject);
     }
-    var testData = {
-        min: 0,
-        max: dataMax,
-        data: jsonData
-    };
-    return testData;
 }
 
 function getMedian(json) {
@@ -108,9 +64,64 @@ function getMedian(json) {
      return data[Math.round(json.length/2)];
 }
 
+function toggleHeatmap() {
+    heatmap.setMap(heatmap.getMap() ? null : map);
+}
+
+function changeGradient() {
+  var gradient = [
+    'rgba(0, 255, 255, 0)',
+    'rgba(0, 255, 255, 1)',
+    'rgba(0, 191, 255, 1)',
+    'rgba(0, 127, 255, 1)',
+    'rgba(0, 63, 255, 1)',
+    'rgba(0, 0, 255, 1)',
+    'rgba(0, 0, 223, 1)',
+    'rgba(0, 0, 191, 1)',
+    'rgba(0, 0, 159, 1)',
+    'rgba(0, 0, 127, 1)',
+    'rgba(63, 0, 91, 1)',
+    'rgba(127, 0, 63, 1)',
+    'rgba(191, 0, 31, 1)',
+    'rgba(255, 0, 0, 1)'
+  ]
+  heatmap.set('gradient', heatmap.get('gradient') ? null : gradient);
+}
+
+function getRadius(value, change) {
+    console.log(">" + value);
+    if(value == undefined){
+        value = null;
+    }
+    var radius = [
+        5,
+        null,
+        20,
+        40,
+        60,
+        80,
+        100
+    ];
+    var i = radius.indexOf(value) + change;
+    
+    if(i >= 0 && i <= radius.length-1){
+        console.log(radius[i]);
+
+        return radius[i];
+    }
+    else{
+        console.log(value);
+
+        return value;
+    }
+}
+
+function changeRadius() {
+  heatmap.set('radius', getRadius(heatmap.get('radius'), 1));
+}
+
+function changeOpacity() {
+  heatmap.set('radius', getRadius(heatmap.get('radius'), -1));
+}
+
 google.maps.event.addDomListener(window, 'load', initialize);
-
-
-/**
- * Created by Stefan Strijker on 9/30/2014.
- */
